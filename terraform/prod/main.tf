31ded1ec93a501f2353eba2e7a9e8cd800f42890
Artem Sereda <overmind88@gmail.com>
2018-07-22 02:09:42 +0300

    Terraform prod for HW-7

---
+++ terraform/prod/main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
+}
+
+module "db" {
+  source              = "../modules/db"
+  public_key_path     = "${var.public_key_path}"
+  zone                = "${var.zone}"
+  database_disk_image = "${var.database_disk_image}"
+}
+
+module "vpc" {
+  source          = "../modules/vpc"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  source_ranges   = ["109.63.136.80/32"]
+}
