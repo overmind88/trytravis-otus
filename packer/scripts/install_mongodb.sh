750f7e9136d6dc282023fffbd1ecc65fdf4d9637
Artem Sereda <overmind88@gmail.com>
2018-07-22 02:05:31 +0300

    Packer scripts refactoring after feedback

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
-sudo apt update
-sudo apt install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt update
+apt install -y mongodb-org
+systemctl enable mongod
