8ba08996f3211ac4631bc49bfae761d02d6b6290
Artem Sereda <overmind88@gmail.com>
2018-08-28 05:31:31 +0300

    HW13* ansible, packer

---
+++ docker-monolith/infra/terraform/variables.tf
@@ -0,0 +1,21 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+
+variable "instance_count" {
+  description = "Number of GCP instances"
+  default     = 2
+}
